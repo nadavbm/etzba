307582a1fc7daff94825732aca2c7d8fdb20338a
nadavbm <nadavbm@outlook.com>
2023-05-19 17:16:16 +0200

    fix and extend example and makefile for postgresql

--- examples/pgsql/seed.sql
+++ examples/pgsql/seed.sql
@@ -1,7 +1,3 @@
-\l
-\dt
-select * from results;
-
 DO $$ DECLARE
 BEGIN
 --
@@ -32,15 +28,18 @@ END IF;
 CREATE TABLE results (
 	id SERIAL PRIMARY KEY NOT NULL,
     created timestamptz NOT NULL DEFAULT NOW(),
-    avg_query_duration DOUBLE PRECISION NOT NULL,
-	min_query_duration DOUBLE PRECISION NOT NULL,
-    med_query_duration DOUBLE PRECISION NOT NULL,
-    max_query_duration DOUBLE PRECISION NOT NULL,
-    total_queries INT NOT NULL
+	type TEXT NOT NULL,
+	job_duration DOUBLE PRECISION NOT NULL,
+    avg_duration DOUBLE PRECISION NOT NULL,
+	min_duration DOUBLE PRECISION NOT NULL,
+    med_duration DOUBLE PRECISION NOT NULL,
+    max_duration DOUBLE PRECISION NOT NULL,
+    total INT NOT NULL
 );
 
 INSERT INTO migrations (name) VALUES ('create-results-table');
 END $$;
+
 
 --
 -- Name: data; Description: add data to results table
@@ -51,35 +50,35 @@ IF
 EXISTS(SELECT 1 FROM migrations WHERE name = 'add-data') THEN RETURN;
 END IF;
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (12.32,56.32,31.14,99.90,10);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('api', 65.65,12.32,56.32,31.14,99.90,10);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (11.12,49.19,32.34,90.91,21);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('api', 45.45,11.12,49.19,32.34,90.91,21);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (13.12,54.43,32.32,87.98,109);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('api', 51.52,13.12,54.43,32.32,87.98,109);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (14.14,56.29,35.87,86.21,230);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('api', 54.54,14.14,56.29,35.87,86.21,230);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (11.23,51.15,38.12,84.32,509);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('api', 56.56,11.23,51.15,38.12,84.32,509);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (14.22,54.11,36.11,81.22,15);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('api', 45.45,14.22,54.11,36.11,81.22,15);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (12.32,56.32,31.14,99.90,67);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('sql', 66.66,12.32,56.32,31.14,99.90,67);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (11.12,49.19,32.34,90.91,107);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('sql', 55.55,11.12,49.19,32.34,90.91,107);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (13.12,54.43,32.32,87.98,279);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('sql', 44.44,13.12,54.43,32.32,87.98,279);
 
-INSERT INTO results (avg_query_duration, min_query_duration, med_query_duration, max_query_duration, total_queries)
-VALUES (14.14,56.29,35.87,86.21,78);
+INSERT INTO results (type, job_duration, avg_duration, min_duration, med_duration, max_duration, total)
+VALUES ('sql', 34.34,14.14,56.29,35.87,86.21,78);
 
 INSERT INTO migrations (name) VALUES ('add-data');
 END $$;
