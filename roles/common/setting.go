165d7a4e4c2e7fdc7ca2705d7d7f06389700c491
Nadav Ben Mazia <nadavbm@outlook.com>
2024-06-21 12:59:12 +0200

    Chore(settings): Refactor scheduler settings

---
+++ roles/common/setting.go
@@ -0,0 +1,49 @@
+package common
+
+import "time"
+
+type Settings struct {
+	// NumberOfWorkers use the "--workers=x" to set the amount of workers while running a load test job
+	NumberOfWorkers int
+	// Duration is how long the command should run (30s, 1m etc)
+	Duration time.Duration
+	// Rps define the frequency for api requests or sql queries during the job execution
+	Rps int64
+	// TasksOrder defined by api request or sql query weight. The weight cann be defined in the config file and order tasks in the worker assignment channel by calculating the weight of each task
+	TasksOrder []int
+	// ExecutionType from command line arg can be sql, api or other type of executions
+	ExecutionType string
+	// ConfigFile used for authentication for api server or sql server
+	ConfigFile string
+	// HelpersFile provided via command line tool and contains all assignments for workers
+	HelpersFile string
+	// Verbose shows worker executions in terminal
+	Verbose bool
+}
+
+func GetSettings(jobDuration time.Duration, exectionType, configFile, helpersFile string, rps, workersCount int, verbose bool) *Settings {
+	s := Settings{
+		Duration:      jobDuration,
+		ExecutionType: exectionType,
+		ConfigFile:    configFile,
+		HelpersFile:   helpersFile,
+		Verbose:       verbose,
+	}
+	if rps != 0 {
+		s.Rps = int64(rps)
+	}
+	if workersCount != 0 {
+		s.NumberOfWorkers = workersCount
+	}
+	return &s
+}
+
+// setRps returns a duration to sleep (in a second timeframe) that will set the amount of requests per seconds later during job execution
+func (s *Settings) SetRps() time.Duration {
+	var rpsSleepInDurationLoop time.Duration
+	if s.Rps == 0 {
+		return rpsSleepInDurationLoop
+	}
+	rpsSleepInDurationLoop = time.Duration(int64(1000/s.Rps) * 1000000)
+	return rpsSleepInDurationLoop
+}
